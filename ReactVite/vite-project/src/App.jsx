2b06a581520679c240b00952d8525b0b3cced430
Tomy Huisa <huisatomy2007@gmail.com>
2024-06-03 16:59:44 -0300

    Nuevo añadido React/Bun

---
+++ ReactVite/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import { MyImage } from "./components/FirstComponent/FirstComponent";
+
+function App() {
+  return (
+    <>
+      <MyImage name={"Tomy"} lastName={"Huisa"} />
+    </>
+  );
+}
+
+export default App;
---
+++ ReactVite/vite-project/src/components/FirstComponent/FirstComponent.jsx
@@ -0,0 +1,8 @@
+export function MyImage({ name, lastName }) {
+  return (
+    <>
+      Hola! , Mi nombre es {name} y mi apellido es {lastName}
+      <img src="./imgs/sunshine.webp" />
+    </>
+  );
+}
